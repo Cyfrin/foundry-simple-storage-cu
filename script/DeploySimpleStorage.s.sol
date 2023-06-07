14569c20f6486b022504ad88261e4db3e20c61e4
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2023-06-06 22:14:52 -0400

    update

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.8.19;
 
-import "forge-std/Script.sol";
-import "../src/SimpleStorage.sol";
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
